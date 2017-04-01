aa8c515e081fc13d3e751cb535b0668d0f610480
Jon Huynh <jhsoccer123@gmail.com>
2017-04-01 19:02:31 -0400

    Sign in ui custom classes complete

---
+++ social-media/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  social-media
+//
+//  Created by Jon Huynh on 4/1/17.
+//  Copyright © 2017 Jon Huynh. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
